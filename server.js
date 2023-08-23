07bd4bd9c06d30f77c4f06b70e4631b03b94cd93
Dhruv <dhruvsuvarna2019@gmail.com>
2023-08-24 00:53:10 +0530

    latest

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+ 
 // Connect to MongoDB using Mongoose
 
 app.get("/", (req, res)=>{
@@ -18,3 +19,16 @@ app.get("/", (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
 });
+
+/* Week1 Objectives(Dhruv and Shibhya)
+1. Translation API (find)
+2. Basic EJS Pages(HTML)
+3. Backend linking
+4. Integrating Translation API
+*/
+
+/* Week1 Objectives(Alfred and Joshua)
+1. Making Basic UI
+2. Learning Frontend
+3. Implementing some Frontend
+*/
